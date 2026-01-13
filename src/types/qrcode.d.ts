81e7472e8aea53419bd61df565020bfa38caec7e
4n0n1mo <bernardo.lima@outlook.com>
2026-01-13 18:55:35 -0300

    Add files via upload

---
+++ src/types/qrcode.d.ts
@@ -0,0 +1,17 @@
+declare module "qrcode" {
+  type QRCodeToDataURLOptions = {
+    margin?: number;
+    width?: number;
+    errorCorrectionLevel?: "L" | "M" | "Q" | "H";
+    color?: {
+      dark?: string;
+      light?: string;
+    };
+  };
+
+  const QRCode: {
+    toDataURL(text: string, options?: QRCodeToDataURLOptions): Promise<string>;
+  };
+
+  export default QRCode;
+}
